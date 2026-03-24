048bf9e9e84d5773b523aeb8da39810724f96357
Brandon Romero <brandonromero1964@gmail.com>
2026-03-23 20:46:34 -0600

    [FIX] Errores de ramas en Playbook.yml de ansible

--- main.tf
+++ main.tf
@@ -26,6 +26,12 @@ resource "digitalocean_firewall" "web" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+  protocol         = "tcp"
+  port_range       = "8080"
+  source_addresses = ["0.0.0.0/0", "::/0"]
+}
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
